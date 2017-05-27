8a11278199734f25e1f4df76b5870edfa692bcc5
Dimitar Jilanov <djilanov@gmail.com>
2017-05-28 01:17:50 +0300

    Fix visual glitches

--- src/app/dictionary/bg.dictionary.ts
+++ src/app/dictionary/bg.dictionary.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 export class BulgarianDictionary {
     public language = {
         "homePage": "Начало",
+        "services": "Видове услуги",
         "contactPage": "Контакти",
         "contactPhone1": "+359(0) 888 234 454",
         "contactPhone2": "+359(0) 888 234 454"
